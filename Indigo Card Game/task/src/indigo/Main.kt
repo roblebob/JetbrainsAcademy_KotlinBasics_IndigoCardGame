c2e9d1c9dbc4d58bfa3dc6ebcd8c7730924efe59
roblebob <robert.koppisch@gmail.com>
2022-09-23 13:47:02 +0200

    finished stage 1/5

--- Indigo Card Game/task/src/indigo/Main.kt
+++ Indigo Card Game/task/src/indigo/Main.kt
@@ -1,5 +1,17 @@
 package indigo
 
+val RANKS = listOf("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
+val SUITS = listOf("♦", "♥", "♠", "♣")
+
+
+
 fun main() {
-    println("Hello World!")
+    println(RANKS.joinToString(" ") + "\n\n" +
+            SUITS.joinToString(" ") + "\n\n" +
+            cartesianProduct(SUITS.slice(listOf(2, 1, 0, 3)), RANKS)
+                .joinToString(" ") { pair -> pair.second + pair.first })
+}
+
+fun <T, U> cartesianProduct(c1: Collection<T>, c2: Collection<U>): List<Pair<T, U>> {
+    return c1.flatMap { lhsElem -> c2.map { rhsElem -> lhsElem to rhsElem } }
 }
